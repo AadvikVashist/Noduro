7790e4ff81085eb463ad0a29902a3bbc02e88467
Nathan Lui <nathanlui@jrnvidybk05.inst.hcpss.org>
2023-05-08 11:22:08 -0400

    changes

--- settings/settings.js
+++ settings/settings.js
@@ -6,15 +6,15 @@ let lastnameVal = localStorage.getItem("lastname");
 let displaynameVal = localStorage.getItem("displayname");
 let darkmodeVal = localStorage.getItem("darkmode");
 
-if (firstnameVal != null) {
-    document.getElementById("firstname").value = firstnameVal;
-}
-if (lastnameVal != null) {
-    document.getElementById("lastname").value = lastnameVal;
-}
-if (displaynameVal != null) {
-    document.getElementById("displayname").value = displaynameVal;
-}
+// if (firstnameVal != null) {
+//     document.getElementById("firstname").value = firstnameVal;
+// }
+// if (lastnameVal != null) {
+//     document.getElementById("lastname").value = lastnameVal;
+// }
+// if (displaynameVal != null) {
+//     document.getElementById("displayname").value = displaynameVal;
+// }
 if (darkmodeVal != null) {
     document.getElementById("darkmode").value = darkmodeVal;
 
@@ -27,30 +27,41 @@ if (darkmodeVal != null) {
     }
 }
 
-document.getElementById("buttons").addEventListener("click", async () => {
-    const firstname = document.getElementById("firstname");
-    const lastname = document.getElementById("lastname");
-    const displayname = document.getElementById("displayname");
-    const darkmode = document.getElementById("darkmode");
-    if (firstname.value.length == 0) {
-        localStorage.setItem("firstname", "");
-    }
-    else{
-        localStorage.setItem("firstname", firstname.value);
-    }
-    if (lastname.value.length == 0) {
-        localStorage.setItem("lastname", "");
-    }
-    else{
-        localStorage.setItem("lastname", lastname.value);
-    }
+document.getElementById("dark_button").addEventListener("click", async () => {
+    window.darkMode.dark();
+});
+document.getElementById("light_button").addEventListener("click", async () => {
+    window.darkMode.light();
+});
+document.getElementById("system_button").addEventListener("click", async () => {
+    window.darkMode.system();
+});
 
-    if (displayname.value.length == 0) {
-        localStorage.setItem("displayname", "");
-    }
-    else{
-        localStorage.setItem("displayname", displayname.value);
-    }
+
+document.getElementById("buttons").addEventListener("click", async () => {
+    // const firstname = document.getElementById("firstname");
+    // const lastname = document.getElementById("lastname");
+    // const displayname = document.getElementById("displayname");
+    const darkmode = document.getElementById("darkmode");
+    // if (firstname.value.length == 0) {
+    //     localStorage.setItem("firstname", "");
+    // }
+    // else{
+    //     localStorage.setItem("firstname", firstname.value);
+    
+    // if (lastname.value.length == 0) {
+    //     localStorage.setItem("lastname", "");
+    // }
+    // else{
+    //     localStorage.setItem("lastname", lastname.value);
+    // }
+
+    // if (displayname.value.length == 0) {
+    //     localStorage.setItem("displayname", "");
+    // }
+    // else{
+    //     localStorage.setItem("displayname", displayname.value);
+    // }
 
     localStorage.setItem("darkmode", darkmode.value);
 
@@ -64,20 +75,3 @@ document.getElementById("buttons").addEventListener("click", async () => {
     }
 });
 
-// document.getElementById("buttons").addEventListener("click", async () => {
-//     const firstname = document.getElementById("firstname");
-//     const lastname = document.getElementById("lastname");
-//     const displayname = document.getElementById("displayname");
-//     const darkmode = document.getElementById("darkmode");
-//     localStorage.setItem("firstname", firstname.value);
-//     localStorage.setItem("lastname", lastname.value);
-//     localStorage.setItem("displayname", displayname.value);
-//     localStorage.setItem("darkmode", darkmode.value);
-//     if (darkmode.value == "Dark") {
-//         await window.darkMode.dark();
-//     } else if (darkmode.value == "Light") {
-//         await window.darkMode.light();
-//     } else {
-//         await window.darkMode.system();
-//     }
-// });
